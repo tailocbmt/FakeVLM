42dae46830f5aa60211aa29f2dda8283eeaa144f
chiisa <locntse150150@fpt.edu.vn>
2026-06-16 17:54:14 +0800

    fix: fix path

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -3,5 +3,5 @@ python scripts/predict.py \
   --val_batch_size 16 \
   --workers 16 \
   --output_path results/fakevlm.json \
-  --test_json_file "./evons_data/test_multilabel.csv" \
-  --data_base_test "./evons_data" \
+  --test_json_file "../aigen-foodreview/evons_data/test_multilabel.csv" \
+  --data_base_test "../aigen-foodreview/evons_data" \
