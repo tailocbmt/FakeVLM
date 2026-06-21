c9619e0ed9d9a574dbcbec3fc9bc298b498d51bb
chiisa <locntse150150@fpt.edu.vn>
2026-06-21 14:01:08 +0800

    fix: fix save path

--- scripts/predict_text.sh
+++ scripts/predict_text.sh
@@ -2,5 +2,5 @@ python scripts/predict_text.py \
   --model_path "anon-review-meld-2026/meld" \
   --val_batch_size 256 \
   --workers 16 \
-  --output_path results/fakevlm.json \
+  --output_path results/fakevlm_text.json \
   --test_json_file "../aigen-foodreview/evons_data/test_multilabel.csv" \
